3905c58c8a0389589b6c1e5035961619bffe963c
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-08 15:38:14 +0800

    add 3d model to hero section

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ npx tailwindcss init -p
 
 Install remaining dependencies:
 ```
-npm i --legacy-peer-deps @react-three/fiber @react-three/drei maath react-tilt react-vertical-timeline-component @emailjs/browser framer-motion react-router-dom
+npm i --legacy-peer-deps three @react-three/fiber @react-three/drei maath react-tilt react-vertical-timeline-component @emailjs/browser framer-motion react-router-dom
 ```
 
 To Run:
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+import CanvasLoader from '../Loader';
 
 const Computers = () => {
+  const { scene } = useGLTF('./desktop_pc/scene.gltf');
+
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadowMapSize={1024}
+      />
+      <primitive 
+        object={scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.02, -0.1]}
+      />
+    </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls 
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas;
