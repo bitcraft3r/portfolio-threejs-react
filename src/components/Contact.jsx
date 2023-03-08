b14af6884823dfa86b1a7011c97c8e2566df84f9
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-08 18:27:14 +0800

    implement Earth 3d model

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -43,6 +43,10 @@ const Contact = () => {
           </button>
         </form>
       </motion.div>
+
+      <motion.div variants={slideIn("right", "tween", 0.2, 1)} className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]">
+        <EarthCanvas />
+      </motion.div>
     </div>
   )
 }
